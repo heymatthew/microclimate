e8d9ec969f0fa27e51add5c2291d5977a0141040
Matthew B. Gray <debug@matthew.nz>
2023-11-02 09:03:17 +1300

    test: Assert static content present

--- web/files_test.go
+++ web/files_test.go
@@ -1,7 +1,6 @@
 package web_test
 
 import (
-	"fmt"
 	"strings"
 	"testing"
 
@@ -12,17 +11,19 @@ import (
 func TestFiles(t *testing.T) {
 	t.Run("errors when file missing", func(t *testing.T) {
 		is := is.New(t)
-
 		_, err := web.Files.ReadFile("missing.txt")
 		is.True(err != nil)
-		fmt.Println("Err is", err)
 	})
 
-	t.Run("can find standard templates", func(t *testing.T) {
+	t.Run("finds static content", func(t *testing.T) {
 		is := is.New(t)
+		_, err := web.Files.ReadFile("static/style.css")
+		is.True(err == nil)
+	})
 
+	t.Run("finds index", func(t *testing.T) {
+		is := is.New(t)
 		index, err := web.Files.ReadFile("templates/index.html.tmpl")
-		fmt.Println("Err is", err)
 		is.True(err == nil)
 		is.True(strings.HasPrefix(string(index), "<!doctype html>"))
 	})
