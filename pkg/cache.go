e2d08a02ecd8ef62584681f5978b52fe7b7212f4
Matthew B. Gray <debug@matthew.nz>
2023-11-06 08:47:53 +1300

    refactor: Extract Sample into separate file
    This was right next to Cache, but it was getting a bit hard to read.
    Extracting to another file should make it easier to keep those separate.

--- pkg/cache.go
+++ pkg/cache.go
@@ -1,29 +1,9 @@
 package pkg
 
 import (
-	"bytes"
 	"os"
 	"path/filepath"
-
-	"github.com/yuin/goldmark"
 )
-
-type Sample struct {
-	Path string
-}
-
-func (s Sample) Content() string {
-	content, err := os.ReadFile(s.Path)
-	if err != nil {
-		panic(err)
-	}
-	var buf bytes.Buffer
-	err = goldmark.Convert(content, &buf)
-	if err != nil {
-		panic(err)
-	}
-	return string(buf.String())
-}
 
 type Cache struct {
 	Dir      string
