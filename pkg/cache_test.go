2cfdab315755bb4ee33149701c254ba19ea8d6a3
Matthew B. Gray <debug@matthew.nz>
2023-11-06 08:56:49 +1300

    test: Move specs for markdown to sample
    Makes sense to keep these close.

--- pkg/cache_test.go
+++ pkg/cache_test.go
@@ -1,14 +1,12 @@
 package pkg_test
 
 import (
-	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
 	"testing"
 
 	"github.com/heymatthew/microclimate/pkg"
-	"github.com/lithammer/dedent"
 	"github.com/matryer/is"
 )
 
@@ -56,27 +54,5 @@ func TestCache(t *testing.T) {
 		is.NoErr(top.Load())
 		is.True(strings.Contains(top.Articles[0].Content(), "aaa"))
 		is.True(strings.Contains(top.Articles[1].Content(), "bbb"))
-	})
-
-	t.Run("translates markdown", func(t *testing.T) {
-		is := is.New(t)
-
-		// Create samples
-		config := t.TempDir()
-		str := dedent.Dedent(`
-			# Heading
-
-			content content content
-		`)
-		path := filepath.Join(config, "test.md")
-		err := os.WriteFile(path, []byte(str), 0644)
-		is.NoErr(err)
-
-		top := pkg.Cache{Dir: config}
-		is.NoErr(top.Load())
-		body := top.Articles[0].Content()
-		fmt.Println(body)
-		is.True(strings.Contains(body, "content content content"))
-		is.True(strings.Contains(body, "<h1>Heading</h1>"))
 	})
 }
